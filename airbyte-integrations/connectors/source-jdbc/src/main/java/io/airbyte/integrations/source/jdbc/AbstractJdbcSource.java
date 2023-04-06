1cbaa0ae361da9b7b0d15f8f8cedb0b4ba011f9d
jdpgrailsdev <jpearlin1@gmail.com>
2023-04-06 16:56:57 -0400

    WIP Track stream status in source

--- airbyte-integrations/connectors/source-jdbc/src/main/java/io/airbyte/integrations/source/jdbc/AbstractJdbcSource.java
+++ airbyte-integrations/connectors/source-jdbc/src/main/java/io/airbyte/integrations/source/jdbc/AbstractJdbcSource.java
@@ -31,6 +31,7 @@ import com.google.common.collect.Sets;
 import datadog.trace.api.Trace;
 import io.airbyte.commons.functional.CheckedConsumer;
 import io.airbyte.commons.json.Jsons;
+import io.airbyte.commons.stream.AirbyteStreamUtils;
 import io.airbyte.commons.util.AutoCloseableIterator;
 import io.airbyte.commons.util.AutoCloseableIterators;
 import io.airbyte.db.JdbcCompatibleSourceOperations;
@@ -309,6 +310,8 @@ public abstract class AbstractJdbcSource<Datatype> extends AbstractDbSource<Data
                                                                final CursorInfo cursorInfo,
                                                                final Datatype cursorFieldType) {
     LOGGER.info("Queueing query for table: {}", tableName);
+    final io.airbyte.protocol.models.AirbyteStreamNameNamespacePair airbyteStream =
+        AirbyteStreamUtils.convertFromNameAndNamespace(tableName, schemaName);
     return AutoCloseableIterators.lazyIterator(() -> {
       try {
         final Stream<JsonNode> stream = database.unsafeQuery(
@@ -349,11 +352,11 @@ public abstract class AbstractJdbcSource<Datatype> extends AbstractDbSource<Data
               return preparedStatement;
             },
             sourceOperations::rowToJson);
-        return AutoCloseableIterators.fromStream(stream);
+        return AutoCloseableIterators.fromStream(stream, airbyteStream);
       } catch (final SQLException e) {
         throw new RuntimeException(e);
       }
-    });
+    }, airbyteStream);
   }
 
   /**
